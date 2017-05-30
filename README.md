1baada56d9aaa85b18327c146c46d435c08ad445
Pratyusha Rayavarapu <pratyusharayavarapu@PR-MacBook-Air.fios-router.home>
2017-05-29 22:05:33 -0400

    Game is working

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # week-4-game
+
+Psuedo Code:
+
+1) Create HTML layout using bootstrap. Include header, instructions box, random number generator box, wins and loses box, crystal buttons (or boxes), total score box
+2) Implement CSS
+2) Implement crystal pictures as buttons
+3) Random number generator 
+4) Assign hidden value to each button
+5) Display total added number 
+	-add to the score based on what crystal is clicked
+6) Create condition statements
+	-If total score is equal to random number then add 1 to wins if number goes above total score then add 1 to losses
+7) After that reset random number generator to new number and total score to 0
+8) Generate random values to crystals again
---
+++ assets/javascript/game.js
@@ -0,0 +1,136 @@
+
+//Variables	
+var totalScore = 0;
+var green_button = Math.floor((Math.random() * 12) + 1);
+var red_button = Math.floor((Math.random() * 12) + 1);
+var blue_button = Math.floor((Math.random() * 12) + 1);
+var yellow_button = Math.floor((Math.random() * 12) + 1);
+var wins = 0;
+var losses = 0;
+
+//Random Number Generator
+var randomNumber = Math.floor((Math.random() * (120-19+1))+19);
+
+$(document).ready(function() {
+	
+	//Display and console.log
+	$("#random-number").text("Random Number: " + randomNumber);
+	console.log(randomNumber);
+
+//On-click Functions
+	$("#green_gem").click(function(){
+		console.log(totalScore);
+		totalScore = totalScore + green_button;
+		$("#total-score").text(totalScore);
+	//Calling scoreboard function
+		score(totalScore,randomNumber);
+	//Display values on console	
+		console.log(green_button);
+		console.log(totalScore);
+
+	})
+
+	$("#red_gem").click(function(){
+		console.log(totalScore);
+		totalScore = totalScore + red_button;
+		$("#total-score").text(totalScore);
+		//Calling scoreboard function
+		score(totalScore,randomNumber);
+		//Display console	
+		console.log(red_button);
+		console.log(totalScore);
+	})
+
+	$("#blue_gem").click(function(){
+		console.log(totalScore);
+		totalScore = totalScore + blue_button;
+		$("#total-score").text(totalScore);
+		//Calling scoreboard function
+		score(totalScore,randomNumber);
+		//Display console
+		console.log(blue_button);
+		console.log(totalScore);
+	})
+
+	$("#yellow_gem").click(function(){
+		console.log(totalScore);
+		totalScore = totalScore + yellow_button;
+		$("#total-score").text(totalScore);
+		//Calling scoreboard function
+		score(totalScore,randomNumber);
+		//Display console
+		console.log(yellow_button);
+		console.log(totalScore);
+	})
+	 
+ //Condition Statements
+
+   console.log("totalscore: " + totalScore);
+
+ //Function to add to scoreboard. A and B are local variables.
+   function score(a,b){
+
+   if(a === b){
+   		wins++;
+   		console.log("wins = " + wins);
+   		//Call the reset function
+   		reset();
+   	}
+
+   	else if (a > b){
+   		losses++;
+   		console.log("losses = " + losses);
+   		//Call the reset function
+   		reset();
+
+   	}
+   
+
+//Adding the wins and losses to the counter
+
+	var html =
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>";
+
+ document.querySelector("#wins-losses").innerHTML = html;
+
+}
+
+
+//Reset function!
+
+	function reset() {
+		totalScore = 0;
+		$("#total-score").text(totalScore);
+		green_button = Math.floor((Math.random() * 12) + 1);
+		console.log("greenbutton:" + green_button);
+		red_button = Math.floor((Math.random() * 12) + 1);
+		blue_button = Math.floor((Math.random() * 12) + 1);
+		yellow_button = Math.floor((Math.random() * 12) + 1);
+		randomNumber = Math.floor((Math.random() * (120-19+1))+19);
+		$("#random-number").text("Random Number: " + randomNumber);
+		console.log("randomnumber:" + randomNumber);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document ready function ending
+})
+	
